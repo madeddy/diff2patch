df9d9753d61a7ea1648a4ed45a385c52a7cf618c
madeddy <mad-eddy@gmx.de>
2023-05-15 11:14:29 +0200

    Add License info and file

--- diff2patch.py
+++ diff2patch.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python3
+
 """
+Copyright 2023 madeddy
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+
+
 Diff2patch is a tool which compares two directorys trees, e.g dir1/dir2, and
 compiles a set of lists with the findings.
 From this result can another directory or archive constructed, which contains all different or in the second dir added objects. A written report is also possible.
 
 Parts of the code of this tool shadows with changes to it pythons filecmp module.
 """
+
+__title__ = 'Diff2patch'
+__license__ = 'Apache 2.0'
+__author__ = 'madeddy'
+__status__ = 'Development'
+__version__ = '0.24.0-alpha'
+__url__ = "https://github.com/madeddy/diff2patch"
+__all__ = ['Log', 'D2pCommon', 'DirTreeCmp', 'D2p']
 
 import sys
 import argparse
@@ -35,17 +60,6 @@ if sys.platform.startswith('win32'):
         else:
             k = windll.kernel32
             k.SetConsoleMode(k.GetStdHandle(-11), 7)
-
-
-__title__ = 'Diff2patch'
-__license__ = 'Apache 2.0'
-__author__ = 'madeddy'
-__status__ = 'Development'
-__version__ = '0.23.0-alpha'
-__url__ = "https://github.com/madeddy/diff2patch"
-
-
-__all__ = ['Log', 'D2pCommon', 'DirTreeCmp', 'D2p']
 
 
 # TODO:
